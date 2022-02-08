994abd13e55823fd4bc322df219a595ce54a07cb
Mark Roduner <mark@energyservices.org>
2022-02-08 12:58:33 -0800

    Script Helper

---
+++ Scripts/timer/helper.js
@@ -0,0 +1,19 @@
+﻿/** provided by: brat-timer-driver */
+var timer = ConnectDriver("TimerDriver");
+
+function Remind(callback, seconds, event = null) {
+    if (!event) event = {};
+    let timer_name = UUID();
+    let timer_info = Persist.Initialize(timer_name, {});
+    timer_info.relay = AddRelay(CompileFilter({
+        event_id: `@${timer_name}`,
+        driver: "TimerDriver"
+    }), callback);
+    timer_info.active = CreateTimer(timer_name, seconds, event);
+    return timer_name;
+
+}
+function CancelRemind(timer_name) {
+    CancelTimer(timer_name);
+    CancelRelay(Persist[timer_name].relay);
+}
